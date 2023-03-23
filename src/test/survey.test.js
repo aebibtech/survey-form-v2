1add1077681202a9fd8581433bd831852c88ed22
aebibtech <paul.camano@gmail.com>
2023-03-23 23:42:35 +0800

    Modified test descriptions

--- src/test/survey.test.js
+++ src/test/survey.test.js
@@ -7,7 +7,7 @@ const SurveyModel           = require('../models/survey.model');
 
 describe("Survey Model", function(){
 
-    it('Should return success message when input captcha matches.', function(){
+    it('Should return success message when input captcha matches....', function(){
         let surveyModel = new SurveyModel();
         let captcha = surveyModel.captcha;
         let result = surveyModel.verifyCaptchaInput(captcha);
@@ -15,7 +15,7 @@ describe("Survey Model", function(){
         expect(result).to.equal("Success! Captcha input matched.");
     });
 
-    it('Should return error message when input captcha does not matched.', function(){
+    it('Should return error message when input captcha does not matched....', function(){
         let surveyModel = new SurveyModel();
         let result = surveyModel.verifyCaptchaInput("random");
 
